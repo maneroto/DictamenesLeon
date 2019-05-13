65ea5046ea2333af1aff3f89cab309bb1a52374d
emmanuel ramirez <maneroto@hotmail.com>
2019-05-12 19:17:11 -0500

    Comprimir imágenes, añadir acción de scroll a nav

---
+++ js/init.js
@@ -0,0 +1,14 @@
+$(document).ready(
+function()
+{
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function() {
+	  var currentScrollPos = window.pageYOffset;
+	  if (prevScrollpos > currentScrollPos) {
+	    document.querySelector("nav").style.top = "0";
+	  } else {
+	    document.querySelector("nav").style.top = "-10vh";
+	  }
+	  prevScrollpos = currentScrollPos;
+	}
+});
