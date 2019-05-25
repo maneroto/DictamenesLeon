dada0fc5d1b5f1e29d65151feebdab93053a0c01
maneroto <maneroto@hotmail.com>
2019-05-25 05:07:06 -0500

    Agregar seccion de contacto

--- js/init.js
+++ js/init.js
@@ -1,6 +1,19 @@
 $(document).ready(
 function()
 {
+	
+	$.fn.goTo = function() {
+	    $('html, body').animate({
+	        scrollTop: $(this).offset().top + 'px'
+	    }, 'low');
+	    return this; // for chaining...
+	}
+	setNavScrollBehavior();
+	setAnchors();
+	setupInputs();
+});
+function setNavScrollBehavior()
+{
 	var prevScrollpos = window.pageYOffset;
 	window.onscroll = function() {
 	  var currentScrollPos = window.pageYOffset;
@@ -11,4 +24,45 @@ function()
 	  }
 	  prevScrollpos = currentScrollPos;
 	}
-});
+}
+function setAnchors()
+{
+	let offers = document.querySelectorAll('.offer');
+	for (let i = 0; i < offers.length; i ++)
+	{
+		console.log(offers[i]);
+		offers[i].addEventListener('click', function()
+		{
+			let currentService = '.service:nth-child('+(i+2)+')';
+			console.log(currentService);
+			$(currentService).goTo();
+		});
+	}
+}
+function setupInputs()
+{
+	clearInputs();
+	let inputs = document.querySelectorAll("input");
+	for (let i = 0; i < inputs.length; i ++)
+	{
+		inputs[i].addEventListener(
+		"focus", function(){
+			this.parentElement.classList.add("focused");
+		}
+		);
+
+		inputs[i].addEventListener(
+		"blur", function()
+		{
+			if (this.value == "") this.parentElement.classList.remove("focused");
+		}
+		);
+	}
+}
+function clearInputs(){
+	let inputs = document.querySelectorAll("input");
+	for (let i = 0; i < inputs.length; i ++)
+	{
+		inputs[i].value = "";
+	}
+}
