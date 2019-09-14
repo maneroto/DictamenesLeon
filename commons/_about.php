f83e3a20af4ce76d0faab9ffc98fc0f928d34461
maneroto <maneroto@hotmail.com>
2019-09-14 14:54:16 -0500

    Change fontawesome link and add new about texts

--- commons/_about.php
+++ commons/_about.php
@@ -4,7 +4,7 @@
 			¿Quiénes somos?
 		</h2>
 		<p>
-			GER-PENSIONES fue originada conceptualmente para dar orientación, tanto a trabajadores como a empresas que deseen ampliar sus conocimientos con respecto al área de pensiones IMSS e ISSSTE, desde su origen que son las AFORES; y cómo tomar decisiones en la vida laboral, así como toda la tramitologia relaciónada a los diferentes tipos de pensiones derivadas, previas a las de retiro.
+			GERPENSIONES fue originada conceptualmente para dar orientación, tanto a trabajadores como a empresas que deseen ampliar sus conocimientos con respecto al área de Pensiones IMSS e ISSSTE, desde su origen e inicio de su vida laboral (AFORES), así como la toma de decisiones para un mejor presente y futuro, INCLUYENDO toda la tramitología relacionada con  los diferentes tipos y ramos de pensión de las diferentes autoridades IMSS e ISSSTE, PREVIAS AL RETIRO.
 		</p>
 	</div>
 	<div class="sliderItem">
@@ -12,7 +12,7 @@
 			¿A qué nos dedicamos?
 		</h2>
 		<p>
-			Damos orientación tanto a trabajadores como a empresas para que puedan ampliar sus conocimientos con respecto al área de pensiones IMSS e ISSSTE, así como también de las Afores y, brindamos asesoría profesional para lograr tramitar una mejor pensión a través de la Modalidad 40 del IMSS ley 73, también podemos ayudar con problemáticas concernientes diversos tipos de seguros existentes en el ámbito profesional. Tenemos servicios de atención a empresas o grupos para cursos y capacitaciones en las diferentes áreas de nuestro conocimiento.
+			Damos orientación tanto a trabajadores como a empresas para que puedan ampliar sus conocimientos con respecto al marco legal de PENSIONES IMSS e ISSSTE, así como capacitación a promotores de AFORES. Otorgamos una asesoría profesional para lograr una mejor PENSIÓN a través de la modalidad 40 y 44 del IMSS ley 1973. Tramitamos pólizas de protección GMM Y auto. Brindamos apoyo a los departamentos de Recursos humanos de las empresas del sector público y privado que coticen a IMSS O ISSSTE dando CAPACITACIONES, CURSOS Y PROGRAMAS DE RETIRO INDIVIDUAL de cada trabajador que nos lo solicite.
 		</p>
 	</div>
 	<div class="sliderItem">
@@ -20,7 +20,7 @@
 			¿Por qué elegirnos?
 		</h2>
 		<p>
-			Tenemos una amplia experiencia de 22 años en el área y estamos enfocados a brindarle un trato profesional que cumpla con sus expectativas. Nuestro objetivo es despejar toda duda que usted pueda tener como nuestro cliente y que quede totalmente satisfecho con nuestro trabajo, el cual pretendemos que sea siempre eficiente y de calidad.
+			Contamos con 22 años de experiencia en el ramo, haciendo un enfoque profesional para brindarle un trato preferencial y que cumpla con SUS expectativas de manera COLECTIVA e INDIVIDUAL, atendiendo a la HONESTIDAD Y LEGALIDAD que nos caracterizan, contamos con instructores y personal calificado para la impartición de los cursos y las asesorías, para la toma de decisiones propias para el bien del trabajador y de sus familias. 
 		</p>
 	</div>
 	<div class="sliderButtons">
--- commons/_headOpen.php
+++ commons/_headOpen.php
@@ -69,4 +69,4 @@
 	<link rel="alternate" hreflang="x-default" href="<?php echo $httpProtocol.$host.$url.$thisPage; ?>" />
 
 	<!-- Font Awesome -->
-	<script src="https://kit.fontawesome.com/0000d6b42d.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h" crossorigin="anonymous">
