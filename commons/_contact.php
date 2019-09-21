01e6e1bd8e8def978242eb53f891e5e26a2c157c
maneroto <maneroto@hotmail.com>
2019-09-21 14:16:55 -0500

    Fix contact text

--- commons/_contact.php
+++ commons/_contact.php
@@ -4,7 +4,7 @@
 	</h2>
 	<div class="contactContent">
 		<div class="form">
-			<h3 class="sectionTitle dark smallTitle">
+			<h3 class="sectionTitle dark">
 				Evaluamos su caso de manera gratuita.
 			</h3>
 			<form class="contactForm">
@@ -31,7 +31,7 @@
 			</form>
 		</div>
 		<div class="map">
-			<h3 class="sectionTitle smallTitle">
+			<h3 class="sectionTitle">
 				Puede visitarnos en nuestras oficinas.
 			</h3>
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25038.043794276884!2d-101.6674936632742!3d21.115481801119984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842bbe5ab16730e1%3A0xce8c2e1aafa63369!2sBlvd.%20Adolfo%20L%C3%B3pez%20Mateos%2C%20Le%C3%B3n%2C%20Gto.!5e0!3m2!1ses!2smx!4v1569091360718!5m2!1ses!2smx" frameborder="0" style="border:0;" allowfullscreen></iframe>
