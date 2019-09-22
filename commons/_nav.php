8d0252b2e9646810e8d0456d8dbc78ae3bbe7854
maneroto <maneroto@hotmail.com>
2019-09-22 15:24:12 -0500

    Add logo anchor

--- commons/_nav.php
+++ commons/_nav.php
@@ -1,6 +1,6 @@
 <nav class="nav">
 	<div class="navContent">
-		<img src="<?php echo $httpProtocol.$host.$url ?>img/Logo.svg" alt="Gerpensiones" class="logo">
+		<img src="<?php echo $httpProtocol.$host.$url ?>img/Logo.svg" alt="Gerpensiones" class="logo" data-target='.bannerSection'>
 		<i class="fas fa-bars mobile toggleMenu" onclick="toggleActive('.navMenu')"></i>
 		<ul class="navMenu">
 			<li data-target = '.aboutSection'>
