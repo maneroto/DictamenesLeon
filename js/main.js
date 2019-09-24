d988e22936fcc03a9a967919ed3fd3260b593d07
maneroto <maneroto@hotmail.com>
2019-09-24 18:57:56 -0500

    Fix some error

--- js/main.js
+++ js/main.js
@@ -13,61 +13,58 @@ window.onresize = function() {
     setBodyPadding()
 };
 
-function setImageObserver()
-{
-	document.addEventListener("DOMContentLoaded", function() {
-	var lazyloadImages;    
+document.addEventListener("DOMContentLoaded", function() {
+var lazyloadImages;    
 
-	if ("IntersectionObserver" in window) 
-	{
-		lazyloadImages = document.querySelectorAll(".lazy");
-		var imageObserver = new IntersectionObserver(function(entries, observer) {
-			entries.forEach(function(entry) 
+if ("IntersectionObserver" in window) 
+{
+	lazyloadImages = document.querySelectorAll(".lazy");
+	var imageObserver = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(entry) 
+		{
+			if (entry.isIntersecting) 
 			{
-				if (entry.isIntersecting) 
-				{
-					var image = entry.target;
-					image.classList.remove("lazy");
-					if (image.nodeName == 'IMG') image.src = image.dataset.src;
-					imageObserver.unobserve(image);
+				var image = entry.target;
+				image.classList.remove("lazy");
+				if (image.nodeName == 'IMG') image.src = image.dataset.src;
+				imageObserver.unobserve(image);
+			}
+		});
+	});
+
+	lazyloadImages.forEach(function(image) {
+		imageObserver.observe(image);
+	});
+} else {  
+	var lazyloadThrottleTimeout;
+	lazyloadImages = document.querySelectorAll(".lazy");
+
+	function lazyload () {
+		if(lazyloadThrottleTimeout) {
+			clearTimeout(lazyloadThrottleTimeout);
+		}    
+
+		lazyloadThrottleTimeout = setTimeout(function() {
+			var scrollTop = window.pageYOffset;
+			lazyloadImages.forEach(function(img) {
+				if(img.offsetTop < (window.innerHeight + scrollTop)) {
+					img.src = img.dataset.src;
+					img.classList.remove('lazy');
 				}
 			});
-		});
-
-		lazyloadImages.forEach(function(image) {
-			imageObserver.observe(image);
-		});
-	} else {  
-		var lazyloadThrottleTimeout;
-		lazyloadImages = document.querySelectorAll(".lazy");
-
-		function lazyload () {
-			if(lazyloadThrottleTimeout) {
-				clearTimeout(lazyloadThrottleTimeout);
-			}    
-
-			lazyloadThrottleTimeout = setTimeout(function() {
-				var scrollTop = window.pageYOffset;
-				lazyloadImages.forEach(function(img) {
-					if(img.offsetTop < (window.innerHeight + scrollTop)) {
-						img.src = img.dataset.src;
-						img.classList.remove('lazy');
-					}
-				});
-				if(lazyloadImages.length == 0) { 
-					document.removeEventListener("scroll", lazyload);
-					window.removeEventListener("resize", lazyload);
-					window.removeEventListener("orientationChange", lazyload);
-				}
-			}, 20);
-		}
-
-		document.addEventListener("scroll", lazyload);
-		window.addEventListener("resize", lazyload);
-		window.addEventListener("orientationChange", lazyload);
+			if(lazyloadImages.length == 0) { 
+				document.removeEventListener("scroll", lazyload);
+				window.removeEventListener("resize", lazyload);
+				window.removeEventListener("orientationChange", lazyload);
+			}
+		}, 20);
 	}
-	});
+
+	document.addEventListener("scroll", lazyload);
+	window.addEventListener("resize", lazyload);
+	window.addEventListener("orientationChange", lazyload);
 }
+});
 
 var setNavScrollBehavior = function() {
         var a = window.pageYOffset;
