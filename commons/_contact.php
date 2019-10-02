33f22521396c5c3412f764067cf6afbb866f45f5
maneroto <maneroto@hotmail.com>
2019-10-02 01:20:28 -0500

    Change iframe to img

--- commons/_contact.php
+++ commons/_contact.php
@@ -35,7 +35,9 @@
 			<h3 class="sectionTitle normalText">
 				Puede visitarnos en nuestras oficinas.
 			</h3>
-			<iframe defer src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25038.043794276884!2d-101.6674936632742!3d21.115481801119984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842bbe5ab16730e1%3A0xce8c2e1aafa63369!2sBlvd.%20Adolfo%20L%C3%B3pez%20Mateos%2C%20Le%C3%B3n%2C%20Gto.!5e0!3m2!1ses!2smx!4v1569091360718!5m2!1ses!2smx" style="border:0;" allowfullscreen></iframe>
+			<a href="https://www.google.com/maps/place/Blvd.+Adolfo+L%C3%B3pez+Mateos,+Le%C3%B3n,+Gto./@21.1173758,-101.6609788,17z/data=!3m1!4b1!4m5!3m4!1s0x842bbe5ab16730e1:0xce8c2e1aafa63369!8m2!3d21.1173758!4d-101.6587901">
+				<img class="lazy" data-src="<?php echo $httpProtocol.$host.$url?>img/contactMap.png" alt='Oficina en guanajuato'></img>
+			</a>
 		</div>
 	</div>
 </section>
