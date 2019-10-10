9b930297859c302ab3696ab6d7c80faa697d5170
maneroto <maneroto@hotmail.com>
2019-10-10 15:50:33 -0500

    Add activity price

--- commons/_activity.php
+++ commons/_activity.php
@@ -69,5 +69,49 @@
 			</div>
 		</div>
 	</div>
+	<div class="activity">
+		<div class="front">
+			<div class="tag">
+				curso
+			</div>
+			<div class="title">
+				Cómo mejorar tu pensión con inversión.
+			</div>
+			<i class="fas fa-hand-point-up icon"></i>
+			<div class="date">
+				26 de octubre del 2019, de 10:00am a 2:00pm. Yucatán.
+			</div>
+		</div>
+		<div class="back">
+			<div class="tag">
+				curso
+			</div>
+			<div class="price">
+				<i class="fas fa-dollar-sign"></i> 1750
+			</div>
+			<div class="title">
+				Cómo mejorar tu pensión con inversión.
+			</div>
+			<div class="description">
+				<ul>
+					<li>
+						Modalidad 40 IMSS
+					</li>
+					<li>
+						Fundamentos 
+					</li>
+					<li>
+						Procedimientos 
+					</li>
+					<li>
+						Casos individuales de los participantes 
+					</li>
+					<li>
+						Proceso de trámite y cobro 
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 
 </div>
