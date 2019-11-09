4ef74737c79ead8363fd23659eb5038f436d26bd
maneroto <maneroto@hotmail.com>
2019-11-09 01:39:31 -0600

    Agregar google analytics

--- commons/_headClose.php
+++ commons/_headClose.php
@@ -5,6 +5,15 @@
 	
 	<!-- Código Urchin -->
 	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151915873-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-151915873-1');
+	</script>
+
 
 </head>
 <body>
