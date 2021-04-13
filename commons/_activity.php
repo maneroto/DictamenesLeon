f430d2e202823531deff2a400d0ee7d8fbe5f2b9
emmanuel ramirez <maneroto@hotmail.com>
2021-04-13 14:42:18 -0500

    Modify conference card by Juan

--- commons/_activity.php
+++ commons/_activity.php
@@ -1,78 +1,71 @@
 <div class="activityContainer">
-	<div class="activity info">
-		<div class="front">
-			<div class="title">
-				Así puede ser parte de nuestras actividades:
-			</div>
-			<i class="fas fa-hand-point-up icon"></i>
-			<div class="date">
-				!No se pierda de estas grandes oportunidades para aprender!
-			</div>
-		</div>
-		<div class="back">
-			<div class="title">
-				Sea parte de nuestras actividades
-			</div>
-			<div class="description">
-				<ul>
-					<li>
-						Deposite el monto de inversión a la cuenta HSBC: 4910 8970 6358 0179.
-					</li>
-					<li>
-						Envíe copia del pago a los correos de contacto y WhatsApp, anexe el nombre del participante.
-					</li>
-					<li>
-						En caso de que el curso sea gratuito, solo es necesario confirmar asistencia.
-					</li>
-					<li>
-						¡Asista!
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="activity other">
-		<div class="front">
-			<div class="tag">
-				conferencia
-			</div>
-			<div class="price">
-				<i class="fas fa-dollar-sign"></i> Gratis
-			</div>
-			<div class="title">
-				Conferencia gratuita: Modalidad 40 IMSS
-			</div>
-			<i class="fas fa-hand-point-up icon"></i>
-			<div class="date">
-				17 de enero del 2020, de 10:00am a 12:30pm. Mérida, Yucatán<br>
-				17 de enero del 2020, de 15:30pm a 17:30pm. Mérida, Yucatán
-			</div>
-		</div>
-		<div class="back">
-			<div class="tag">
-				conferencia
-			</div>
-			<div class="title">
-				¿Eres trabajador activo ante el IMSS?, ¿Cotizaste por lo menos 10 años con un patrón?.
-			</div>
-			<div class="description">
-				<ul>
-					<li>
-						Modalidad 40 (continuación voluntaria en el régimen obligatorio)
-					</li>
-					<li>
-						Análisis individual de cada participante
-					</li>
-				</ul>
-				<br>
-				<p>
-					Canacintra, Salón BEPENSA, calle 30, por 7 y 7A #151, Colonia García Gineres, Mérida, Yucatán.
-				</p>
-				<br>
-				<p>
-					Expositores: Germán Chávez Velázquez y Wilbert Jiménez Montejo
-				</p>
-			</div>
-		</div>
-	</div>
+    <div class="activity info">
+        <div class="front">
+            <div class="title">
+                Así puede ser parte de nuestras actividades:
+            </div>
+            <i class="fas fa-hand-point-up icon"></i>
+            <div class="date">
+                !No se pierda de estas grandes oportunidades para aprender!
+            </div>
+        </div>
+        <div class="back">
+            <div class="title">
+                Sea parte de nuestras actividades
+            </div>
+            <div class="description">
+                <ul>
+                    <li>
+                        Deposite el monto de inversión a la cuenta HSBC: 4910 8970 6358 0179.
+                    </li>
+                    <li>
+                        Envíe copia del pago a los correos de contacto y WhatsApp, anexe el nombre del participante.
+                    </li>
+                    <li>
+                        En caso de que el curso sea gratuito, solo es necesario confirmar asistencia.
+                    </li>
+                    <li>
+                        ¡Asista!
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="activity other">
+        <div class="front">
+            <div class="tag">
+                conferencia
+            </div>
+            <div class="title">
+                Conferencia: Modalidad 40 IMSS
+            </div>
+            <i class="fas fa-hand-point-up icon"></i>
+        </div>
+        <div class="back">
+            <div class="tag">
+                conferencia
+            </div>
+            <div class="title">
+                ¿Eres trabajador activo ante el IMSS?, ¿Cotizaste por lo menos 10 años con un patrón?.
+            </div>
+            <div class="description">
+                <ul>
+                    <li>
+                        Modalidad 40 (continuación voluntaria en el régimen obligatorio)
+                    </li>
+                    <li>
+                        Análisis individual de cada participante
+                    </li>
+                </ul>
+                <br>
+                <p>
+                    Canacintra, Salón BEPENSA, calle 30, por 7 y 7A #151, Colonia García Gineres, Mérida, Yucatán.
+                </p>
+                <br>
+                <p>
+                    Expositores: Germán Chávez Velázquez y Wilbert Jiménez Montejo
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
--- commons/_headOpen.php
+++ commons/_headOpen.php
@@ -4,7 +4,6 @@
 	$httpProtocol = 'https://';
 	$host = $_SERVER['SERVER_NAME'];
     $url = '/';
-    //$url = '/strix/Gerpensiones/';
     $thisPage = $_SERVER['PHP_SELF'];
     $siteName = "Gerpensiones";
     $author = 'Strix Development/maneroto';
@@ -22,56 +21,65 @@
     ';
 ?>
 <html lang="es" prefix="og: http://ogp.me/ns#">
+
 <head>
-	<title><?php if (isset($title)) echo $title; else echo $siteName ?></title>
+    <title><?php if (isset($title)) echo $title; else echo $siteName ?></title>
 
-	<!-- Metas generales -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-	<meta name="description" content="<?php if (isset($description))echo $description; else echo $siteName; ?>" />
-	<meta name="keywords" content="<?php if (isset($keywords))echo $keywords; else echo $siteName; ?>" />
+    <!-- Metas generales -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="<?php if (isset($description))echo $description; else echo $siteName; ?>" />
+    <meta name="keywords" content="<?php if (isset($keywords))echo $keywords; else echo $siteName; ?>" />
 
-	<!--Metas GEO-->
-	<meta name="geo.region" content="MX-GUA" />
-	<meta name="geo.placename" content="Le&oacute;n" />
-	<meta name="geo.position" content="21.122245;-101.673277" />
-	<meta name="ICBM" content="21.122245, -101.673277" />
+    <!--Metas GEO-->
+    <meta name="geo.region" content="MX-GUA" />
+    <meta name="geo.placename" content="Le&oacute;n" />
+    <meta name="geo.position" content="21.122245;-101.673277" />
+    <meta name="ICBM" content="21.122245, -101.673277" />
 
-	<!--Metas OG-->
-	<meta property="og:locale" content="es_MX" />
-	<meta property="og:title" content="<?php if (isset($title)) echo $title; else echo $siteName ?>" />
-	<meta property="og:description" content="<?php if (isset($description))echo $description; else echo $siteName; ?>" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="<?php echo $httpProtocol.$host.$url; ?>" />
-	<meta property="og:image" content="<?php echo $httpProtocol.$host.$url; ?>img/logo.svg" />
-	<meta property="og:site_name" content="<?php echo $siteName?>" />
+    <!--Metas OG-->
+    <meta property="og:locale" content="es_MX" />
+    <meta property="og:title" content="<?php if (isset($title)) echo $title; else echo $siteName ?>" />
+    <meta property="og:description"
+        content="<?php if (isset($description))echo $description; else echo $siteName; ?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="<?php echo $httpProtocol.$host.$url; ?>" />
+    <meta property="og:image" content="<?php echo $httpProtocol.$host.$url; ?>img/logo.svg" />
+    <meta property="og:site_name" content="<?php echo $siteName?>" />
 
-	<!--Metas DC-->
-	<meta content='<?php if (isset($title)) echo $title; else echo $siteName ?>' NAME='DC.Title'/>
-	<meta content='<?php if (isset($description))echo $description; else echo $siteName; ?>' NAME='DC.Description'/>
-	<meta content='<?php echo $author?>' NAME='DC.Creator'/>
-	<meta content="<?php echo $siteName?>" NAME='DC.Publisher'/>
-	<meta content='<?php echo $httpProtocol.$host.$url; ?>' NAME='DC.Identifier'/>
-	<meta content='<?php if (isset($keywords))echo $keywords; else echo $siteName; ?>' NAME='DC.keywords'/>
+    <!--Metas DC-->
+    <meta content='<?php if (isset($title)) echo $title; else echo $siteName ?>' NAME='DC.Title' />
+    <meta content='<?php if (isset($description))echo $description; else echo $siteName; ?>' NAME='DC.Description' />
+    <meta content='<?php echo $author?>' NAME='DC.Creator' />
+    <meta content="<?php echo $siteName?>" NAME='DC.Publisher' />
+    <meta content='<?php echo $httpProtocol.$host.$url; ?>' NAME='DC.Identifier' />
+    <meta content='<?php if (isset($keywords))echo $keywords; else echo $siteName; ?>' NAME='DC.keywords' />
 
-	<!--FAVICON-->
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/favicon-16x16.png">
-	<link rel="manifest" href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/site.webmanifest">
-	<link rel="mask-icon" href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/safari-pinned-tab.svg" color="#5bbad5">
-	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="theme-color" content="#ffffff">
+    <!--FAVICON-->
+    <link rel="apple-touch-icon" sizes="180x180"
+        href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo $httpProtocol.$host.$url; ?>img/Favicon/safari-pinned-tab.svg"
+        color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 
-	<!-- Author -->
-	<meta name="author" content="<?php echo $author; ?>">
+    <!-- Author -->
+    <meta name="author" content="<?php echo $author; ?>">
 
-	<!--HREFLANG-->
-	<link rel="alternate" hreflang="x-default" href="<?php echo $httpProtocol.$host.$url.$thisPage; ?>" />
+    <!--HREFLANG-->
+    <link rel="alternate" hreflang="x-default" href="<?php echo $httpProtocol.$host.$url.$thisPage; ?>" />
 
-	<!-- Font Awesome -->
-	<link rel="preload" href="<?php echo $httpProtocol.$host.$url?>css/webfonts/fa-solid-900.woff2" as="font" type="font/woff2" crossorigin>
-	<link rel="preload" href="<?php echo $httpProtocol.$host.$url?>css/webfonts/fa-brands-400.woff2" as="font" type="font/woff2" crossorigin>
-	<link rel="stylesheet" type="text/css" href="<?php echo $httpProtocol.$host.$url?>css/Fontawesome/fontawesome.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo $httpProtocol.$host.$url?>css/Fontawesome/brands.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo $httpProtocol.$host.$url?>css/Fontawesome/solid.min.css">
+    <!-- Font Awesome -->
+    <link rel="preload" href="<?php echo $httpProtocol.$host.$url?>css/webfonts/fa-solid-900.woff2" as="font"
+        type="font/woff2" crossorigin>
+    <link rel="preload" href="<?php echo $httpProtocol.$host.$url?>css/webfonts/fa-brands-400.woff2" as="font"
+        type="font/woff2" crossorigin>
+    <link rel="stylesheet" type="text/css"
+        href="<?php echo $httpProtocol.$host.$url?>css/Fontawesome/fontawesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $httpProtocol.$host.$url?>css/Fontawesome/brands.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo $httpProtocol.$host.$url?>css/Fontawesome/solid.min.css">
